a10992ba5b358294492f01c8407bf686f81f591c
Manucho40 <rickypaine40@gmail.com>
2024-03-26 13:57:10 +0000

    Delete back folder

--- back/src/api/type-app/controllers/type-app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * type-app controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::type-app.type-app');
--- back/src/api/type-app/routes/type-app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * type-app router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::type-app.type-app');
